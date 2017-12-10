979a4dec1b193c0e72bc9bfc4d5ae0fe05ffec6e
akanekotyui <a_roid_ba_sky@yahoo.co.jp>
2017-12-10 17:29:27 +0900

    抵抗値をはかる

--- happyStairs.ino
+++ happyStairs.ino
@@ -1,9 +1,37 @@
+
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // アナログ入力の値を電圧(V)に変換
+  float voltage = (analogRead(0) / 1024.0) * 5.0;
+ 
+  // 誤差を除去
+  if(voltage >= 4.9){
+    voltage = 4.99;
+  }
+  
+  Serial.print("入力電圧：");
+  Serial.print(voltage);
+  Serial.print("V 抵抗値：");
+ 
+  // 圧力センサーの抵抗値を出力する
+  if(voltage >= 4.9){
+     Serial.println("1MΩ ");
+  }else{
+    float val = voltage / (5.0 - voltage);
+    if(val > 999){
+      val = val / 1000;
+      Serial.print(val,0);
+      Serial.print("k");      
+    }else{
+      Serial.print(val, 0);
+    }    
+    Serial.println("Ω ");        
+  }   
+  
+  delay(100);
 }
+
